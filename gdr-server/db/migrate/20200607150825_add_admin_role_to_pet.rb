44ad1dc559dd6c4ec08a60807ef9ff7f12ee0990
saramccombs <theSaraMcCombs@gmail.com>
2020-06-07 16:37:12 -0400

    init(database): Add admin to User table
    Add boolean admin to user table.
    
    Remove admin from pet table as it was added by mistake to that table rather than the user table.

--- gdr-server/db/migrate/20200607150825_add_admin_role_to_pet.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminRoleToPet < ActiveRecord::Migration[6.0]
-  def change
-    add_column :pets, :admin, :boolean
-  end
-end
--- gdr-server/db/schema.rb
+++ gdr-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_150825) do
+ActiveRecord::Schema.define(version: 2020_06_07_201347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_06_07_150825) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_150825) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
   end
 
 end
