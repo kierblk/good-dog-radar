2b3f1eb8d8746f6ee3bdd116612ca242cbf0b3f9
saramccombs <theSaraMcCombs@gmail.com>
2020-06-07 16:38:56 -0400

    init(backend): Update Controllers with Default Actions
    Update controllers with full crud default actions to be edited at a future point.

--- gdr-server/app/controllers/api/v1/medications_controller.rb
+++ gdr-server/app/controllers/api/v1/medications_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::MedicationsController < ApplicationController
+  before_action :set_medication, only: [:show, :update, :destroy]
+  
+  # GET /medications
+  def index
+    @medications = Medication.all
+
+    render json: @medication
+  end
+
+  # GET /medications/1
+  def show
+    render json: @medication
+  end
+
+  # POST /medications
+  def create
+    @medication = Medication.new(medication_params)
+
+    if @medication.save
+      render json: @medication, status: :created, location: @medication
+    else
+      render json: @medication.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /medications/1
+  def update
+    if @medication.update(medication_params)
+      render json: @medication
+    else
+      render json: @medication.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /medications/1
+  def destroy
+    @medication.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_medication
+      @medication = Medication.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def medication_params
+      params.require(:medication).permit(:med_name, :med_reason, :med_instructions, :med_frequency, :med_dosage, :med_prescriber_name, :med_prescriber_phone)
+    end
 end
--- gdr-server/app/controllers/api/v1/pets_controller.rb
+++ gdr-server/app/controllers/api/v1/pets_controller.rb
@@ -1,6 +1,51 @@
 class Api::V1::PetsController < ApplicationController
-
+  before_action :set_pet, only: [:show, :update, :destroy]
+  
+  # GET /pets
   def index
-    render plain: "Hello World!"
+    @pets = User.all
+
+    render json: @pets
   end
+
+  # GET /pets/1
+  def show
+    render json: @pet
+  end
+
+  # POST /pets
+  def create
+    @pet = Pet.new(pet_params)
+
+    if @pet.save
+      render json: @pet, status: :created, location: @pet
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /pets/1
+  def update
+    if @pet.update(pet_params)
+      render json: @pet
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /pets/1
+  def destroy
+    @pet.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def pet_params
+      params.require(:pet).permit(:organization_id, :user_id, :name, :breed, :birthdate, :photo)
+    end
 end
