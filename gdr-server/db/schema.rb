abb40b44d3c5e93a5dfd5198b1d6fa0cb1bf24c2
saramccombs <theSaraMcCombs@gmail.com>
2020-06-06 23:18:04 -0400

    refac(database): Create DB and Run Migrations
    Created DB via rails db:create. Create schema via rails db:migrate.

---
+++ gdr-server/db/schema.rb
@@ -0,0 +1,68 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_06_07_020740) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "pet_id"
+    t.integer "user_id"
+    t.datetime "comment_date"
+    t.string "comment_body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "medications", force: :cascade do |t|
+    t.string "med_name"
+    t.string "med_reason"
+    t.string "med_instructions"
+    t.string "med_frequency"
+    t.string "med_dosage"
+    t.string "med_prescriber_name"
+    t.string "med_prescriber_phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "contact_person"
+    t.string "contact_email"
+    t.string "contact_phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.integer "organization_id"
+    t.integer "user_id"
+    t.string "name"
+    t.string "breed"
+    t.date "birthdate"
+    t.string "photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "organization_id"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
