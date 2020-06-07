f428ecb8c6e2e85e248f937b66f92d65c2f1022d
saramccombs <theSaraMcCombs@gmail.com>
2020-06-06 23:21:06 -0400

    refac(backend): Update Routes with Namespacing
    Update routes to utilize /api/v1 namespacing.

--- gdr-server/config/routes.rb
+++ gdr-server/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+      resources :medications
+      resources :organizations
+      resources :pets
+      resources :users
+    end
+  end
 end
