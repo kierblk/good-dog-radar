9658b474607d3b676a839a4c70b04f02a75c74d1
saramccombs <theSaraMcCombs@gmail.com>
2020-06-11 16:28:35 -0400

    init(database): Add Username and Password to User
    Create a migration to add username and password to the user model.

---
+++ gdr-server/db/migrate/20200610223359_add_username_and_password_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameAndPasswordToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :password_digest, :string
+  end
+end
