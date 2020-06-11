0ed2b2ba94d85f79c6532d8d0d83ddedeed6164a
saramccombs <theSaraMcCombs@gmail.com>
2020-06-11 16:38:47 -0400

    refac(seeds.rb): Create seed data
    Create user, pet, and organization seed data to verify models.

--- gdr-server/db/seeds.rb
+++ gdr-server/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test_org = Organization.create({
+  name: "Org 1",
+  contact_person: "Sara",
+  contact_email: "sara@email.com",
+  contact_phone: "123-456-7890"
+})
+
+radar = Pet.create({
+  name: "Radar",
+  breed: "Bull Terrier",
+  user_id: 1,
+  organization_id: 1
+})
+
+sara = User.create({
+  name:"Sara",
+  email: "sara@email.com",
+  phone: "123-456-7890",
+  admin: true,
+  organization_id: 1,
+  username: "sara",
+  password: "password"
+})
