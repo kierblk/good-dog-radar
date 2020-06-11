e4007d08fefe3f33f119e9bd65888da1bc6124a7
saramccombs <theSaraMcCombs@gmail.com>
2020-06-11 16:59:23 -0400

    refac(users_controller.rb): Update index action to render serialized json

--- gdr-server/app/controllers/api/v1/users_controller.rb
+++ gdr-server/app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,11 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    # render json: @users
+    # json_string = MovieSerializer.new(movie).serialized_json
+      users_json = UserSerializer.new(@users).serialized_json
+
+      render json: users_json
   end
 
   # GET /users/1
