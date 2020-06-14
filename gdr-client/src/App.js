f3ff74164e9d11d3ba7f9f47a5c3d7340d52f744
saramccombs <theSaraMcCombs@gmail.com>
2020-06-14 10:39:41 -0400

    refac(App.js): Add Login component
    Add Login component, comment out header to remove style but not delete it as it might be useful in the future.

--- gdr-client/src/App.js
+++ gdr-client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      {/* <header className="App-header">
+        <Login />
+      </header> */}
+      <Login />
     </div>
   );
 }
