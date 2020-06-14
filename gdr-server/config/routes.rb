8196fd087bed37b9be9c550d9c2ee77e186822b8
saramccombs <theSaraMcCombs@gmail.com>
2020-06-14 11:50:33 -0400

    init(sessions): Create sessions_controller.rb
    Create sessions_controller.rb and add login route to routes.rb.

--- gdr-server/config/routes.rb
+++ gdr-server/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/api/v1/login', to: "sessions#create"
+
   namespace :api do
     namespace :v1 do
       resources :comments
