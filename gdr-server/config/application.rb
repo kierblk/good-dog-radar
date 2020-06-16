7f499c0cecd5447adc292af6cd54fcfdc963baaa
saramccombs <theSaraMcCombs@gmail.com>
2020-06-16 13:13:37 -0400

    refac(application.rb): Add Middleware for Cookie use for sessions

--- gdr-server/config/application.rb
+++ gdr-server/config/application.rb
@@ -33,5 +33,8 @@ module GdrServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActionDispatch::Session::CookieStore, key:
+    '_cookie_name'
   end
 end
