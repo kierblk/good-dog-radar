ab62cd4fde5f5d579ca742e693d0c9ccc68a7c95
saramccombs <theSaraMcCombs@gmail.com>
2020-06-16 13:25:52 -0400

    refac(App.js): Change App to Class, Refac Header with Login and Logout

--- gdr-client/src/App.js
+++ gdr-client/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login'
+import Logout from './components/Logout'
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <Login />
-      </header> */}
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          { this.props.currentUser ? <Logout /> : <Login /> }
+    {/* <Logout /> */}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
