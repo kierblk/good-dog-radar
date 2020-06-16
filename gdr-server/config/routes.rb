a800bb5bd65d0f7e23c9f6d1d230eab67d1b6ce4
saramccombs <theSaraMcCombs@gmail.com>
2020-06-16 13:16:45 -0400

    refac(routes.rb): Update Create Action, Build Current User and Destroy Actions

--- gdr-server/config/routes.rb
+++ gdr-server/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/api/v1/login', to: "sessions#create"
+  post '/api/v1/login', to: 'api/v1/sessions#create'
+  get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
+  delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
 
   namespace :api do
     namespace :v1 do
