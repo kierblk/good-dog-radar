30ce9a094f638d48d867facfe1eca185f508adc0
saramccombs <theSaraMcCombs@gmail.com>
2020-06-16 13:13:00 -0400

    refac(sessions_controller.rb): Build Create, Get Current User, and Destroy Actions
    Build create action for user sign-in, build get_current_user to check session for a user already signed in, build destroy to remove a user from the session.

--- gdr-server/app/controllers/api/v1/sessions_controller.rb
+++ gdr-server/app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: 'Invalid Credentials'
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "Not logged in."
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      notice: "successfully logged out"
+    }, status: :ok
+  end
 end
