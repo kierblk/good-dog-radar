fc25092f9e4c0b2acbd7941e7f84fdc64ab5eccb
saramccombs <theSaraMcCombs@gmail.com>
2020-06-17 14:23:40 -0400

    refac(loginFormn.js): Add RESET_LOGIN_FORM case statement to reducer.

--- gdr-client/src/reducers/loginForm.js
+++ gdr-client/src/reducers/loginForm.js
@@ -7,6 +7,8 @@ const loginForm = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_LOGIN_FORM':
       return action.formData
+    case 'RESET_LOGIN_FORM':
+      return initialState
     default:
       return state
   }
