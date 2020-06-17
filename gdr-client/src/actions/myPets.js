402617c222df5d92a1b8b81a94e046b4984a44f9
saramccombs <theSaraMcCombs@gmail.com>
2020-06-17 17:41:52 -0400

    refac(myPets.js): Add getMyPets functionality.

--- gdr-client/src/actions/myPets.js
+++ gdr-client/src/actions/myPets.js
@@ -4,3 +4,24 @@ export const setMyPets = pets => {
     pets
   }
 }
+
+export const getMyPets = () => {
+  return dispatch => {
+    return fetch('http://localhost:3001/api/v1/pets', {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(pets => {
+      if (pets.error) {
+        alert(pets.error)
+      } else {
+        dispatch(setMyPets(pets.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
