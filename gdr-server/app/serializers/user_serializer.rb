b2fd6774879e2d063f52ca1842d8d444a6436aaa
saramccombs <theSaraMcCombs@gmail.com>
2020-06-17 14:25:15 -0400

    refac(user_serializer.rb): Add custom attribute fo the organization.

--- gdr-server/app/serializers/user_serializer.rb
+++ gdr-server/app/serializers/user_serializer.rb
@@ -1,4 +1,12 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username, :name, :email, :phone, :admin, :organization
+  attributes :username, :name, :email, :phone, :admin
+  attribute :org_info do |user| 
+    {
+      name: user.organization.name,
+      contact: user.organization.contact_person,
+      phone: user.organization.contact_phone,
+      email: user.organization.contact_email
+    }
+  end
 end
