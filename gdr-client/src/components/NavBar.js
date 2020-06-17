b5a8ec81e07fa0eb3be75ab7db05a82fb444be02
saramccombs <theSaraMcCombs@gmail.com>
2020-06-17 14:22:54 -0400

    refac(NavBar.js): Update component
    Update comp[onent to render data from its new structure after implementing a custom serializer for the organization information.

--- gdr-client/src/components/NavBar.js
+++ gdr-client/src/components/NavBar.js
@@ -6,8 +6,12 @@ import Logout from './Logout'
 const NavBar = ({currentUser}) => {
   return (
     <div className="navbar">
-      { currentUser ? <h3>Welcome Back, {currentUser.name}!</h3> : '' }
-      { currentUser ? <Logout /> : <Login /> }
+      { currentUser ? <h1 className="text-white">{currentUser.attributes.org_info.name}</h1> : '' }
+  { currentUser ? <p>Contact: {currentUser.attributes.org_info.contact} - Phone: {currentUser.attributes.org_info.phone} - Email: {currentUser.attributes.org_info.email}</p> : '' }
+      <div className="navbar-align-end">
+        { currentUser ? <p className="text-white">Welcome Back, {currentUser.attributes.name}! </p> : '' }
+        { currentUser ? <Logout /> : <Login /> }
+      </div>
     </div>
   )
 }
