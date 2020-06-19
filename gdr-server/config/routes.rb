a6b7b359be7ef995aab4b92e7a419f58579c05c2
saramccombs <theSaraMcCombs@gmail.com>
2020-06-18 21:11:06 -0400

    refac(routes.rb): Add signup route

--- gdr-server/config/routes.rb
+++ gdr-server/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/api/v1/login', to: 'api/v1/sessions#create'
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
+  post '/api/v1/signup', to: 'api/v1/users#create'
 
   namespace :api do
     namespace :v1 do
