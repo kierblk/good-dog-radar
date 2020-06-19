ad0f6d6ebd7de32dddc551e9ba38405afb886147
saramccombs <theSaraMcCombs@gmail.com>
2020-06-18 22:03:45 -0400

    refac(NavBar.js): Refactor to account for Home component and only render Logout component

--- gdr-client/src/components/NavBar.js
+++ gdr-client/src/components/NavBar.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Login from './Login'
 import Logout from './Logout'
 
 const NavBar = ({currentUser}) => {
   return (
     <div className="navbar">
-      { currentUser ? <h1 className="text-white">{currentUser.attributes.org_info.name}</h1> : '' }
-  { currentUser ? <p>Contact: {currentUser.attributes.org_info.contact} - Phone: {currentUser.attributes.org_info.phone} - Email: {currentUser.attributes.org_info.email}</p> : '' }
+      { 
+      currentUser ? 
+      <h1 className="text-white">{currentUser.attributes.org_info.name}</h1> :
+      '' 
+      }
+
+      { 
+      currentUser ? 
+      <h6 className="text-white">Contact: {currentUser.attributes.org_info.contact} - Phone: {currentUser.attributes.org_info.phone} - Email: {currentUser.attributes.org_info.email}</h6> : 
+      '' 
+      }
+
       <div className="navbar-align-end">
-        { currentUser ? <p className="text-white">Welcome Back, {currentUser.attributes.name}! </p> : '' }
-        { currentUser ? <Logout /> : <Login /> }
+        { 
+        currentUser ? 
+        <>
+          <div className="text-white">Welcome Back, {currentUser.attributes.name}!</div>
+          <div><Logout /></div>
+        </> : 
+        '' 
+        }
       </div>
     </div>
   )
