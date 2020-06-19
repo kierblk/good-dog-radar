b1af6f0f30534993bf7f0328123ace932ff5d125
saramccombs <theSaraMcCombs@gmail.com>
2020-06-18 21:16:38 -0400

    refac(users_controller.rb): Comment out unused actions, Update Create Action to create a new user upon signup.

--- gdr-server/app/controllers/api/v1/users_controller.rb
+++ gdr-server/app/controllers/api/v1/users_controller.rb
@@ -2,45 +2,49 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   
   # GET /users
-  def index
-    @users = User.all
+  # def index
+  #   @users = User.all
 
-    # render json: @users
-    # json_string = MovieSerializer.new(movie).serialized_json
-      users_json = UserSerializer.new(@users).serialized_json
+  #   # render json: @users
+  #   # json_string = MovieSerializer.new(movie).serialized_json
+  #     users_json = UserSerializer.new(@users).serialized_json
 
-      render json: users_json
-  end
+  #     render json: users_json
+  # end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      response = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: response, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -50,6 +54,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest)
+      params.require(:user).permit(:name, :username, :password, :organization_id)
     end
 end
