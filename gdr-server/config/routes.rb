d10bdc39f26bac2ef76c990ef2cc7491bc9c0c03
saramccombs <theSaraMcCombs@gmail.com>
2020-06-20 15:39:25 -0400

    refac(routes.rb): Modify route to nest comments under pets, remove medications routes for the time being.

--- gdr-server/config/routes.rb
+++ gdr-server/config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :comments
-      resources :medications
       resources :organizations
-      resources :pets
+      resources :pets do
+        resources :comments
+      end
       resources :users
     end
   end
