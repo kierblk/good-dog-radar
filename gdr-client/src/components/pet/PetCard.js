f4240f9ac7e99e9b75fa314b2bf9d0c4c9c0064d
saramccombs <theSaraMcCombs@gmail.com>
2020-06-20 15:35:13 -0400

    init(PetCard.js): Create PEtCard to for pet show route.

---
+++ gdr-client/src/components/pet/PetCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PetCard = ({ pet }) => {
+  return (
+    pet ?
+    <>
+      <div className="pet-edit-delete">
+          <Link to={`/pets/${pet.id}/edit`}>Edit</Link> &nbsp;
+          <Link to={`/pets/${pet.id}/delete`}>Delete</Link>
+      </div>
+      <div className="PetCard">
+        <div className="pet-photo">
+          <img src={pet.attributes.photo} alt="" />
+        </div>
+
+        <div className="pet-info">
+          <p className="pet-name">{ pet.attributes.name }</p>
+          <p>{ pet.attributes.breed }</p>
+          <p>{ pet.attributes.birthdate }</p>
+        </div>
+      </div>
+      <div className="pet-comments">
+        <h3>Comments</h3>
+        <Link to="">Add Comment</Link>
+      </div>
+    </>:
+    <p>Add a Pet First!</p>
+  )
+}
+
+export default PetCard
