4a1ae2e5c7d26f58e8212d37d80d06c13813ba24
saramccombs <theSaraMcCombs@gmail.com>
2020-06-20 15:34:31 -0400

    refac(PEt.js): Add thumbnail image, Add Link to pet show route.

--- gdr-client/src/components/pet/Pet.js
+++ gdr-client/src/components/pet/Pet.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Pet = ({ pet }) => {
   return (
-    <div className="Pet">
-      {pet.attributes.name}
-    </div>
+    <Link to={`/pets/${pet.id}`} key={pet.id}>
+      <div className="Pet">
+        <img src={pet.attributes.photo} alt="" /> &nbsp;
+        {pet.attributes.name}
+      </div>
+    </Link>
   )
 }
 
