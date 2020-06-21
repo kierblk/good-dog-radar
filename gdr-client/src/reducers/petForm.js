861e2b5a612fff2458fa780b35bef5087c4f7966
saramccombs <theSaraMcCombs@gmail.com>
2020-06-20 21:02:36 -0400

    rename reducers

--- gdr-client/src/reducers/petForm.js
+++ gdr-client/src/reducers/petForm.js
@@ -7,10 +7,12 @@ const initialState = {
 
 export default (state=initialState, action) => {
   switch (action.type) {
-    case "UPDATE_NEW_PET_FORM":
+    case "UPDATE_PET_FORM":
       return action.formData
-    case "RESET_NEW_PET_FORM":
+    case "RESET_PET_FORM":
       return initialState
+    case "SET_FORM_DATA_FOR_EDIT":
+      return action.petFormData
     default:
       return state
   }
