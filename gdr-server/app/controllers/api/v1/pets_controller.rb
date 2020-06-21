121ad3679b063b8b87dec443ef172702aa53cb5c
saramccombs <theSaraMcCombs@gmail.com>
2020-06-20 20:59:58 -0400

    refac(pets_contoller.rb): Uncomment update action.

--- gdr-server/app/controllers/api/v1/pets_controller.rb
+++ gdr-server/app/controllers/api/v1/pets_controller.rb
@@ -33,13 +33,13 @@ class Api::V1::PetsController < ApplicationController
   end
 
   # PATCH/PUT /pets/1
-  # def update
-  #   if @pet.update(pet_params)
-  #     render json: @pet
-  #   else
-  #     render json: @pet.errors, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @pet.update(pet_params)
+      render json: PetSerializer.new(@pet)
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /pets/1
   # def destroy
